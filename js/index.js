032b72f4f5b321ea1124f69b872c14c84bccf81d
nucikk <kirianuci@gmail.com>
2024-02-18 13:19:39 +0400

    add comments to functions file

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// default ენის შემოწმება
+//^REVIEW -  default ენის შემოწმება
 const checkLang = () => {
     let defaultLang = "ka";
     let currentLang = getData("lang");
@@ -9,67 +9,28 @@ const checkLang = () => {
     return currentLang;
 }
 
-// ენის დაყენების ფუნქციონირების გადართვა
+//^REVIEW -  ენის შეცვლის გადართვა
 const changeLang = () => {
     let currentLang = getData("lang");
     let newLang = currentLang === "ka" ? "en" : "ka";
     setData("lang", newLang);
     generateLangs();
 }
-const processHTMLForEntry = (entry, entriesContainer) => {
-    let entryContainer = document.createElement("div");
-    entryContainer.classList.add("entry");
-    let entryTitle = document.createElement("h3");
-    entryTitle.classList.add("entry-title");
-    entryTitle.innerText = entry.title;
-    let entryContent = document.createElement("p");
-    entryContent.classList.add("entry-content");
-    entryContent.innerText = entry.content;
-    entryContainer.appendChild(entryTitle);
-    entryContainer.appendChild(entryContent);
-    entriesContainer.appendChild(entryContainer);
-}
 
-const generateEntries = () => {
-    let entries = getData("entries");
-    let entriesContainer = document.getElementById("entries");
-
-    if(entries) {
-        entriesContainer.innerHTML = "";
-        entries.forEach((entry) => {
-            processHTMLForEntry(entry, entriesContainer);
-        })
-    }
-}
-// ენების შეცვლის ფუნქცია
-const generateLangs = () => {
-    let lang = checkLang();
-    let addNewEntry = document.getElementById("addNewEntry");
-    let newEntryTitle = document.getElementById("newEntryTitle");
-    let newEntryContent = document.getElementById("newEntryContent");
-    let submitNewEntry = document.getElementById("submitNewEntry");
-    let changeLang = document.getElementById("changeLang");
-
-    addNewEntry.innerText = langs[lang].addNewEntry;
-    newEntryTitle.setAttribute('placeholder', langs[lang].newEntryTitle);
-    newEntryContent.setAttribute('placeholder', langs[lang].newEntryContent);
-    submitNewEntry.innerText = langs[lang].submitNewEntry;
-    changeLang.innerText = lang;
-
-}
-
-
+//^REVIEW -  popupის გახსნა 
 const addNewEntry = () => {
     let newEntryModal = document.getElementById("newEntryModal");   
      document.body.classList.add("modal-open");
     newEntryModal.style.display = "flex";
 }
-
+//^REVIEW -  popupის დახურვა 
 const closeModal = () => {
     let newEntryModal = document.getElementById("newEntryModal");
     document.body.classList.remove("modal-open");
     newEntryModal.style.display = "none";
 }
+
+//^REVIEW -  ახალი ჩანაწერის გაგზავნა localStorage-ში
 const submitNewEntry = () => {
     let newEntryTitle = document.getElementById("newEntryTitle");
     let newEntryContent = document.getElementById("newEntryContent");
@@ -93,10 +54,52 @@ const submitNewEntry = () => {
 
 }
 
+//^REVIEW -  დასამუშავებელი ფუნქცია ქმნის ელემენტებს და ჩასვამს სათანადო ტექსტს
+const processHTMLForEntry = (entry, entriesContainer) => {
+    let entryContainer = document.createElement("div");
+    entryContainer.classList.add("entry");
+    let entryTitle = document.createElement("h3");
+    entryTitle.classList.add("entry-title");
+    entryTitle.innerText = entry.title;
+    let entryContent = document.createElement("p");
+    entryContent.classList.add("entry-content");
+    entryContent.innerText = entry.content;
+    entryContainer.appendChild(entryTitle);
+    entryContainer.appendChild(entryContent);
+    entriesContainer.appendChild(entryContainer);
+}
 
+//^REVIEW -  იღებს ყველა ჩანაწერს და დაუკავშირებს კონტაინერში
+const generateEntries = () => {
+    let entries = getData("entries");
+    let entriesContainer = document.getElementById("entries");
 
+    if(entries) {
+        entriesContainer.innerHTML = "";
+        entries.forEach((entry) => {
+            processHTMLForEntry(entry, entriesContainer);
+        })
+    }
+}
 
-//REVIEW -  ელემენტებზე ტექსტის მინიჭების ფუნქცია
+//^REVIEW -  ენების შეცვლის ფუნქცია სატანადო თარგმანით
+const generateLangs = () => {
+    let lang = checkLang();
+    let addNewEntry = document.getElementById("addNewEntry");
+    let newEntryTitle = document.getElementById("newEntryTitle");
+    let newEntryContent = document.getElementById("newEntryContent");
+    let submitNewEntry = document.getElementById("submitNewEntry");
+    let changeLang = document.getElementById("changeLang");
+
+    addNewEntry.innerText = langs[lang].addNewEntry;
+    newEntryTitle.setAttribute('placeholder', langs[lang].newEntryTitle);
+    newEntryContent.setAttribute('placeholder', langs[lang].newEntryContent);
+    submitNewEntry.innerText = langs[lang].submitNewEntry;
+    changeLang.innerText = lang;
+
+}
+
+//^REVIEW - * მთავარი ფუნაცია აკონტროლებს ჩატვირთვას, ენებს და ჩანაწერებს
 const general = () => {
     generateLangs();
     generateEntries();
--- js/storage.js
+++ js/storage.js
@@ -3,7 +3,7 @@ const exists = (key) => {
     return localStorage.getItem(key) !== null;
     
 }
-// ამოიღებს მოცემულ გასაღებთან დაკავშირებულ მნიშვნელობას localStorage-დან
+// წამოიღებს მოცემულ key-ს დაკავშირებულ მნიშვნელობას localStorage-დან
 const getData = (key) => {
    if(exists(key)) {
     return JSON.parse(localStorage.getItem(key)); //NOTE - JSON.parse-ის გამოყენებით  აბრუნებს მიღებულ ობიექტს
